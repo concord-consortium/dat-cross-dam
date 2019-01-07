b8207d057152159cd15149cea1b007636973392a
dalove1025 <dalove1025@gmail.com>
2019-01-07 12:39:29 -0500

    WIP: Fix minor style issue reported by lint

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -17,8 +17,7 @@ export class AppComponent extends BaseComponent<IProps, IState> {
     return (
       <div className="app">
         <Text text={ui.sampleText} />
-        <canvas id="canvas_for_cartoon" width="600" height="340">
-        </canvas>
+        <canvas id="canvas_for_cartoon" width="600" height="340" />
       </div>
     );
   }
