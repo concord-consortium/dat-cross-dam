18b9b89659442ba259bfec83a466d5028d33e340
dalove1025 <dalove1025@gmail.com>
2019-01-28 16:44:26 -0500

    [ks/ei/dl] Cypress test for "labels" control

--- cypress/integration/workspace.test.ts
+++ cypress/integration/workspace.test.ts
@@ -5,7 +5,37 @@ context("Test the overall app", () => {
 
   describe("Desktop functionalities", () => {
     it("renders with text", () => {
-      cy.get(".app").should("exist");
+      cy.get("#app")
+        .should("exist");
     });
   });
+
+  describe('Test UI functionalities', () => {
+
+    const toggle = "input#label-toggle";
+    const svgText = "svg > text";
+
+    it("defaults to showing the labels", () => {
+      cy.get(toggle)
+        .should("be.visible")
+        .and("checked");
+      cy.get(svgText)
+        .first()
+        .should("be.visible")           // Spot-check that at least one label
+        .and("contain", "Farm River");  // is visible and has the right text.
+    });
+
+    it("user can hide/un-hide labels over the image", () => {
+      cy.get(toggle)                    // First click should hide labels.
+        .click();
+      cy.get(svgText)
+        .should("be.not.visible");
+      cy.get(toggle)                    // Second click to un-hide.
+        .click();
+      cy.get(svgText)
+        .should("be.visible");
+    });
+
+  });
+
 });
